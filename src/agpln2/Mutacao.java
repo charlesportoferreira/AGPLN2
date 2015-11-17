6365f328276430bb76b815a57fe9e709a9cb5bc1
charles <charles.ferreira@aluno.ufabc.edu.br>
2015-11-17 20:07:32 -0200

    primeiro commit

---
+++ src/agpln2/Mutacao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agpln2;
+
+/**
+ *
+ * @author charleshenriqueportoferreira
+ */
+public class Mutacao {
+
+    public static Cromossomo muta(Cromossomo c, int posicao) {
+        if (posicao >= c.getGenes().size() || posicao < 0) {
+            throw new RuntimeException("Posicao para mutacao fora do range de genes");
+        }
+        Cromossomo clone = new Cromossomo(c.getGenes().size());
+        for (int i = 0; i < c.getGenes().size(); i++) {
+            clone.getGenes().get(i).setValor(c.getGenes().get(i).getValor());
+        }
+
+        if (clone.getGenes().get(posicao).getValor() == 1) {
+            clone.getGenes().get(posicao).setValor(0);
+        } else {
+            clone.getGenes().get(posicao).setValor(1);
+        }
+       
+        
+        
+        return clone;
+        //c.getConfigGenes();
+    }
+
+}
